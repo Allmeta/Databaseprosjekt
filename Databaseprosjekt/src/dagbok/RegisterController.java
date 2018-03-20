225f5a66e7f63f342a5f08f4797e35c432b5a560
gisleste <gisleste@gmail.com>
2018-03-20 11:45:52 +0100

    fx greier

--- Databaseprosjekt/src/dagbok/RegisterController.java
+++ Databaseprosjekt/src/dagbok/RegisterController.java
@@ -1,5 +1,72 @@
 package src.dagbok;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
 public class RegisterController {
-/*life*/
+
+    @FXML
+    private TextField Passord;
+
+    @FXML
+    private TextField Pnr;
+
+    @FXML
+    private Button Submit;
+    
+    @FXML
+    private Button cancel;
+
+    @FXML
+    private TextField Fornavn;
+
+    @FXML
+    private TextField Etternavn;
+
+    @FXML
+    private TextField Brukernavn;
+
+    @FXML
+    void CancelReg(ActionEvent event) {
+    	Stage stage = (Stage) cancel.getScene().getWindow();
+        stage.close();
+    	
+
+    }
+
+    @FXML
+    void SubmitReg(ActionEvent event) {
+    	
+    	Window owner = Submit.getScene().getWindow();
+        
+    	if(Fornavn.getText().isEmpty() || Etternavn.getText().isEmpty()) {
+    		AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
+                "Please enter your name");
+    		return;
+    }
+    
+
+    
+    
+    if(Passord.getText().isEmpty()) {
+        AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
+                "Please enter a password");
+        return;
+    }
+
+    
+    
+    AlertHelper.showAlert(Alert.AlertType.CONFIRMATION, owner, "Registration Successful!", 
+            "Welcome " + Fornavn.getText());
+    
+    
+    }
+    
 }
+
+
