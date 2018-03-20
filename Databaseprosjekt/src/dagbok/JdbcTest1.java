d614b93b398801b9bf376198b9b0ac6a420c025b
Allmeta <thomasdasd@gmail.com>
2018-03-20 10:43:22 +0100

    fixed I guess

--- Databaseprosjekt/src/dagbok/JdbcTest1.java
+++ Databaseprosjekt/src/dagbok/JdbcTest1.java
@@ -10,9 +10,10 @@ public class JdbcTest1 {
 			Class.forName("com.mysql.jdbc.Driver");
 
 			// Step 2: Establish the connection to the database
-			String url = "jdbc:msql://mysql.stud.ntnu.no/thomalme_124";
+			String url = "jdbc:mysql://mysql.stud.ntnu.no/thomalme_124";
 
 			Connection conn = DriverManager.getConnection(url, "thomalme", "test1");
+			System.out.println("Worked nais");
 		} catch (Exception e) {
 			System.err.println("D'oh! Got an exception!");
 			System.err.println(e.getMessage());
